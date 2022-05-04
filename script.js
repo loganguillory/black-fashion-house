d2c08fcc24167328e4501e4ae1cf9c56e7860208
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-05-04 14:51:46 -0400

    attempt at click event

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+
+function addEventListener() {
+    document.getElementById("h3");
+    h3.addEventListener('click', function() {
+    alert('I was clicked!')
+    document.querySelectorAll("body").appendChild(h3);
+   });    
+}
